c3e0dd71f06bfedb1152521ba7bbe44c2064a122
Anju Balakrishnan <anjoos2006@gmail.com>
2024-03-05 14:13:08 +0800

    Accessing multiple datatypes using YUL

---
+++ DataTypes.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.2 <0.9.0;
+
+contract YulTypes {
+    function numberType() external pure returns(uint256) {
+        uint256 x;
+
+        assembly {
+            // YUL doesnt need a semicolon at the end of statements
+            // Its like a shared scope here. X can actually reach out of the scope and access the value
+            x := 88
+        }
+
+        return x;
+    }
+
+}
