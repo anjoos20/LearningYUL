0ec8ac8afd6f17bc7cf5a142334a01100171629d
Anju Balakrishnan <anjoos2006@gmail.com>
2024-03-06 19:55:55 +0800

    mstore opcode and the effect

--- Memory.sol
+++ Memory.sol
@@ -20,4 +20,20 @@ NOTE
 */
 
 contract YulMemory {
+//mstore function takes the location that you want to start saving the data in memory from-location 
+// here, from location 0, I am going to save my data which is a s2 byte word 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+    function mstoreExample() public pure {
+        assembly {
+            // moves a 32byte word to memory location 0x0 
+            // 0x0 to 0x1F is the first 32 bytes of memory and the next starts at 0x20
+            mstore(0x0, 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff)
+            // Moves a 1 byte word to memory location 0x1
+            mstore(0x0, 0x0) // reset
+            // Since it gets only 31 bytes in the first memory, it takes up 1 more byte in the next memory 
+            // starting 0x20
+            // Check the screenshots of the remix debug
+            mstore(0x1, 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff)
+        }
+    }
+    
 }
